385113a662636c90852b69ed99afcb7a679cae26
Farrouault-Julien <julien.farrouault@gmail.com>
2023-07-04 15:18:03 +0200

    1

--- projet/src/App.svelte
+++ projet/src/App.svelte
@@ -1,10 +1,21 @@
 <script>
+  import About from "./lib/about.svelte";
 
 </script>
 
 <main>
-	<h1>Template Svelte</h1>
+	<header id="home">
+        <nav id="mebu-bar">
+            <ul>
+                <li><a href="#"><img id="logo" src="/white-logo.png" alt="logo"></a></li>
+                <li class="links"><a href="#">Nous contacter</a></li>
+                <li class="links"><a href="#">Qui-sommes nous</a></li>
+                <li class="links"><a href="#">Creations</a></li>
+            </ul>
+        </nav>
+    </header>
 </main>
+
 
 <style>
 
