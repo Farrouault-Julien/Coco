70ae97d433730913f5d7c272c590533057bcd931
Farrouault-Julien <julien.farrouault@gmail.com>
2023-07-08 10:00:21 +0200

    home page

--- projet/src/lib/ilot.svelte
+++ projet/src/lib/ilot.svelte
@@ -1,35 +1,47 @@
 <div id="home_container">
+
   <img id="logo" src="white-logo.png" />
 
-  <div id="video_bloc">
-    <video autoplay muted loop id="myVideo">
-      <source src="./public/bags_video.mov" type="video/mp4" />
-    </video>
-  </div>
+  <img id="img1" src="output.PNG" />
+  <img id="img2" src="photo-output 8.PNG" />
+  <img id="img3" src="photo-output 9.PNG" />
+
+
 </div>
 
 <style>
 
   #home_container {
       display: flex;
-      float: right;
+      justify-content: space-around;
       width: 100%;
-      height: 100vh;
-      border: 1px solid black;
+      height: 90vh;
       
-  }  
+  } 
+  
+ 
   #logo {
     margin-top: 15%;
     height: 200px;
+    position: absolute;
+    left: 50px;
   }
 
-  #myVideo {
-      max-width: 400px;
-  }
   @media only screen and (max-width: 600px) {
     #logo {
-      width: 60%;
-      margin-left: 20%;
+      width: 80%;
+      
     }
+
+    #img1 {
+      visibility: hidden;
+    }
+    #img2 {
+      visibility: hidden;
+    }
+    #img3 {
+      visibility: hidden;
+    }
+
   }
 </style>
