1160a013f7d1e6a0534fc6af1aa02dd35513b547
Farrouault-Julien <julien.farrouault@gmail.com>
2023-08-01 18:01:12 +0200

    Update Contact.svelte

--- projet/src/lib/Contact.svelte
+++ projet/src/lib/Contact.svelte
@@ -16,20 +16,22 @@
       </div>
       <div class="contact-bloc">
         <h3>
-          Ou bien sur Dinan dans la boutique chez coco au 14 rue de l’horloge
-          22100 Dinan
+          Ou bien sur Dinan dans la boutique chez coco
         </h3>
-        <h4>lien vers Maps ci-dessous :</h4>
-
+        <h4 id="before-icon">lien vers Maps ci-dessous :</h4>
+        <a class="arrow" href="https://goo.gl/maps/kUKt1fMeCEdXaBYC6"
+        ><i
+          class="fa-solid fa-map-location-dot fa-beat-fade fa-4x"
+          style="color: #ffffff;"
+        /></a>
       </div>
       <div class="contact-bloc">
         <h3>
           Également, vous pouvez passer commande directement en DM sur
-          @cocoprendlethe
+          <a href="https://www.instagram.com/cocoprendlethe/"><p>@cocoprendlethe</p></a>
         </h3>
-        <h4>lien vers l'instagram d'Élodie</h4>
-        <h4>mail :<mailto /></h4>
-        <h4>tel :</h4>
+        <h4>mail : <a href="mailto:elodie.cornec@hotmail.fr">elodie.cornec@hotmail.fr</a>
+        <h4>tel : 0672512390</h4>
       </div>
       </div>
       
@@ -60,17 +62,18 @@
   .contact-main-bloc {
     display: flex;
     align-items: center;
-    margin-top: 10%;
+    margin-top: 5%;
   }
 
   .contact-bloc {
     padding-top: 20px;
+    padding-bottom: 20px;
     width: 40%;
     font-size: 15px;
   }
 
   img {
-    width: 100px;
+    width: 200px;
   }
 
   h3 {
@@ -80,8 +83,44 @@
   }
 
   .info-main-bloc {
-    margin-left: 35%;
+    margin-left: 25%;
+    border-radius: 25px;
+    border: 1px solid rgb(255, 255, 254);
+    padding-top: 30px;
+    padding-bottom: 30px;
+    padding-left: 40px;
+    padding-right: 40px;
 
+  }
+  a {
+      text-decoration: none;
+      color: rgb(255, 255, 254);
+    }
+
+  .arrow {
+    padding-top: 10px;
+  }
+
+  #before-icon {
+    margin-bottom: 20px;
+  }
+  @media (min-width: 743px) {
+    .contact-bloc {
+      width: 100%;
+    }
+    h3 {
+      text-align: start; 
+      font-size: 20px;
+      font-weight: bold;
+    }
+    h4 {
+      font-size: 20px;
+    }
+    .arrow {
+      margin-left: 30%;
+    }
+
+    
   }
 
 </style>
